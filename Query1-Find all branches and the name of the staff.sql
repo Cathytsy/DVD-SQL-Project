589dc451c29d82d329c83211ab9f18ca3a4f8189
Cathytsy <147212218+Cathytsy@users.noreply.github.com>
2023-10-15 12:10:21 +0800

    Delete Query1-Find all branches and the name of the staff.sql

--- Query1-Find all branches and the name of the staff.sql
+++
@@ -1,5 +0,0 @@
--- Prompt: Find all branches and the name of the staff
-SELECT staff.staff_id, staff.first_name, staff.last_name, store.store_id, store.manager_staff_id
-FROM staff
-JOIN store
-ON staff.staff_id = store.store_id
