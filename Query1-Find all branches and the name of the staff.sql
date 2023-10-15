b1ec4e0008ad53e24eed393b00258d78425cc6ef
Cathytsy <147212218+Cathytsy@users.noreply.github.com>
2023-10-16 05:14:19 +0800

    Add files via upload

---
+++ Query1-Find all branches and the name of the staff.sql
@@ -0,0 +1,5 @@
+-- Prompt: Find all branches and the name of the staff
+SELECT staff.staff_id, staff.first_name, staff.last_name, store.store_id, store.manager_staff_id
+FROM staff
+FULL JOIN store
+ON staff.staff_id = store.store_id;
